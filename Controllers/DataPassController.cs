cf2a848e9d08376a162acb2b034a4911b4df86c7
Nihat Aydın <nihatydin1@gmail.com>
2025-10-22 17:24:00 +0300

    View'a veri aktarma yöntemleri eklendi.

---
+++ Controllers/DataPassController.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_basics.Controllers
+{
+    public class DataPassController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Model ile view'a veri aktarma
+        public class Users 
+        {
+            public int Id { get; set; }
+            public string? Name { get; set; }
+        }
+        public IActionResult Model()
+        {
+            var users = new List<Users>
+            {
+                new Users { Id = 1, Name = "Ahmet" },
+                new Users { Id = 2, Name = "Zeynep" }
+            };
+            return View(users);
+        }
+
+        // ViewBag ile view'a veri aktarma
+        public IActionResult Viewbag()
+        {
+            ViewBag.Message = "Hello from ViewBag!";
+            return View();
+        }
+
+        // ViewData ile view'a veri aktarma
+        public IActionResult Viewdata()
+        {
+            ViewData["Message"] = "Hello from ViewData!";
+            return View();
+        }
+
+        // TempData ile view'a veri aktarma
+        public IActionResult Tempdata()
+        {
+            return View();
+        }
+        public class TempdataModel
+        {
+            public string? Name { get; set; }
+            public string? Email { get; set; }
+        }
+        [HttpPost]
+        public IActionResult Tempdata(TempdataModel tModel)
+        {
+            TempData["Message"] = $"TempData ile  {tModel.Name} adlı kullanıcıdan gelen e-posta adresi: {tModel.Email}";
+            return RedirectToAction("Index");
+        }
+        
+        
+    }
+}
